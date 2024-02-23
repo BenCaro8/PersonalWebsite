b13ec4073cd6487e7397762da0e1ec43bfdc5361
BenCaro8 <bccaro8@gmail.com>
2024-02-23 14:09:54 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-3xl font-bold underline text-red-600">
+          Simple React Typescript Tailwind Sample
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
