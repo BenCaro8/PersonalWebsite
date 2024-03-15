45e5fa910e9a2b21cef3cc9bed78a65ed214fa65
BenCaro8 <bccaro8@gmail.com>
2024-03-15 18:10:20 -0400

    Added ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import { FC } from 'react';
 import styles from './styles/App.scss';
 
 const App: FC = () => {
-    return (
-        <div>
-            <div className={styles.title}>CSS module works!</div>
-            <div
-                className={
-                    'border-[10px] border-solid border-red-800 rounded-full w-[200px] h-[200px] flex items-center justify-center text-center'
-                }
-            >
-                Tailwind works!
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className={styles.title}>CSS module works!</div>
+      <div
+        className={
+          'border-[10px] border-solid border-red-800 rounded-full w-[400px] h-[400px] flex items-center justify-center text-center text-4xl'
+        }
+      >
+        Tailwind Works!
+      </div>
+    </div>
+  );
 };
 
 export default App;
