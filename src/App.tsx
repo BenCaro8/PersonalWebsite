a5b657110ba01660ea637b7f710c8ad800580be8
BenCaro8 <bccaro8@gmail.com>
2024-03-15 20:50:20 -0400

    Added Prettier Things and Start of Nav Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { FC } from 'react';
+import NavBar from './Components/NavBar';
 import styles from './styles/App.scss';
 
 const App: FC = () => {
   return (
-    <div>
-      <div className={styles.title}>CSS module works!</div>
-      <div
-        className={
-          'border-[10px] border-solid border-red-800 rounded-full w-[400px] h-[400px] flex items-center justify-center text-center text-4xl'
-        }
-      >
-        Tailwind Works!
+    <div className={styles.appContainer}>
+      <NavBar />
+      <div>
+        <div className={styles.title}>Ben Caro: Software Engineer</div>
+        <div
+          className={
+            'border-[10px] border-solid border-red-800 rounded-full w-[400px] h-[400px] flex items-center justify-center text-center text-4xl'
+          }
+        >
+          Tailwind Works!
+        </div>
       </div>
     </div>
   );
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import styles from './styles/NavBar.scss';
+
+const NavBar: FC = () => {
+  return (
+    <div className={styles.container}>
+      Nav
+    </div>
+  );
+};
+
+export default NavBar;
