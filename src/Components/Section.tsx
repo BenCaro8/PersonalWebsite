be75d9a1b35e00460f5a23e8ba43218e11d04e5d
Benji Caro <benjamincaro8@gmail.com>
2024-03-21 15:06:24 -0400

    Added Selfie and Floating Shapes

---
+++ src/Components/Section.tsx
@@ -0,0 +1,32 @@
+import { FC, ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+  backgroundColor?: string;
+  height?: number;
+  zIndex?: number;
+};
+
+const Section: FC<Props> = ({
+  children,
+  backgroundColor = 'primary-bg-color',
+  height,
+  zIndex,
+}) => {
+  return (
+    <section
+      className="w-full flex relative"
+      style={{
+        height: height ? `${height}px` : '100%',
+        backgroundColor: `var(--${backgroundColor})`,
+        zIndex: zIndex || 1,
+      }}
+    >
+      <div className="mx-auto my-auto max-w-screen-2xl w-full px-5 flex">
+        {children}
+      </div>
+    </section>
+  );
+};
+
+export default Section;
--- src/Components/Settings.tsx
+++ src/Components/Settings.tsx
@@ -1,14 +1,32 @@
 import { FC, useState } from 'react';
-import { SketchPicker, ColorChangeHandler } from 'react-color';
+import { ChromePicker, ColorResult } from 'react-color';
+
+type ColorState = {
+  [color: string]: string;
+};
 
 const NavBar: FC = () => {
-  const [backgroundColor, setBackgroundColor] = useState('#181a1b');
+  const [colors, setColors] = useState<ColorState>({});
 
-  const onColorChange: ColorChangeHandler = (color) => {
-    setBackgroundColor(color.hex);
+  const onColorChange = (color: ColorResult, property: string) => {
+    setColors({ ...colors, [property]: color.hex });
+    document.documentElement.style.setProperty(property, color.hex);
   };
 
-  return <SketchPicker color={backgroundColor} onChange={onColorChange} />;
+  return (
+    <div className="float-right text-white">
+      Background Color:
+      <ChromePicker
+        color={colors['--primary-bg-color']}
+        onChange={(color) => onColorChange(color, '--primary-bg-color')}
+      />
+      Accent Color:
+      <ChromePicker
+        color={colors['--primary-accent-color']}
+        onChange={(color) => onColorChange(color, '--primary-accent-color')}
+      />
+    </div>
+  );
 };
 
 export default NavBar;
