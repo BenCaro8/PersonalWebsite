ab731d44cb4d53132984723240935c5752aeac62
BenCaro <76133585+BenCaro8@users.noreply.github.com>
2024-08-13 15:30:12 -0400

    Delete src/Components/styles/NavBar.scss.d.ts

--- src/Components/styles/NavBar.scss.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const navBoxShadow: string;
-export declare const navContainer: string;
-export declare const navLink: string;
-export declare const settingsCog: string;
-export declare const settingsNavLink: string;
