b251eb77a8ec39c71a63c5756dc95e42da223e58
BenCaro8 <bccaro8@gmail.com>
2024-09-01 12:58:49 -0400

    Added Nav to Home Page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import AnimatedBackground from '../Components/AnimatedBackground';
 import Section from '../Components/Section';
 import Card from '../Components/Card';
-import Title from '../Components/Title';
 import TypingAnimation from '../Components/TypingAnimation';
 import styles from './styles/Home.scss';
 
@@ -14,18 +13,56 @@ const Home: FC = () => {
         <img src="/public/self.png" className={styles.selfie} />
       </Section>
       <div className="mt-auto mb-auto">
-        <Section style="py-5" center showAnimatedBackground>
+        <Section style="py-5" center showAnimatedBackground flexCol>
           <Card
             center
-            borderWidth={1}
+            title="About Me"
+            borderWidth={0}
             borderRadius={10}
-            backgroundColor="primary-bg-color"
-            imgSrc="https://cdn-icons-png.flaticon.com/512/8743/8743996.png"
+            backgroundColor="secondary-accent-color"
+            gradient="primary-gradient-color"
+            link="/about"
           >
-            <Title size="medium" center>
-              Explore my website!
-            </Title>
-            <p>(This is a placeholder)</p>
+            <p>Learn about me, my interests and professional experience!</p>
+          </Card>
+          <Card
+            center
+            title="My Résumé"
+            borderWidth={0}
+            borderRadius={10}
+            backgroundColor="secondary-accent-color"
+            link="/resume"
+          >
+            <p>
+              My resume for anyone interested in what I&apos;ve done
+              professionally in a concise format.
+            </p>
+          </Card>
+          <Card
+            center
+            title="Projects"
+            borderWidth={0}
+            borderRadius={10}
+            backgroundColor="primary-gradient-color"
+            gradient="secondary-accent-color"
+            link="/projects"
+          >
+            <p>
+              Take a look at the projects that I&apos;ve done and am currently
+              working on.
+            </p>
+          </Card>
+          <Card
+            center
+            title="Themes"
+            borderWidth={0}
+            borderRadius={10}
+            backgroundColor="primary-accent-color"
+            link="/themes"
+          >
+            <p>
+              Change the look of this website by playing around with the themes.
+            </p>
           </Card>
         </Section>
       </div>
